ec3960c49cefc1ea16ad6464850b5d193b017680
jonayethossen <jonayethossen8921@gmail.com>
2026-01-04 15:47:49 +0600

    Button add

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,8 +1,20 @@
+import Link from "next/link";
+import BlogItem from "../components/BlogItem";
+import blogs from "../data/blog";
+
 export default function Blogs() {
   return (
     <div className="p-8">
-      <h1 className="text-xl"></h1>
-      <div className="mt-6 p-4">{blogs}</div>
+      <h1 className="text-[60px] font-bold">Blog item</h1>
+      <div className="mt-6 p-4">
+        {blogs.map((blog) => (
+          <BlogItem key={blog.id} title={blog.title} />
+        ))}
+      </div>
+      <Link href={"/"} className="btn btn-primary">
+        {" "}
+        Home
+      </Link>
     </div>
   );
 }
---
+++ app/components/BlogItem.js
@@ -0,0 +1,3 @@
+export default function BlogItem({ title }) {
+  return <div>{title}</div>;
+}
