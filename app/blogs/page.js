f3b1d608a87c5be4dc8e2cc9e1bfe91684e7fbd4
jonayethossen <jonayethossen8921@gmail.com>
2026-01-05 23:40:14 +0600

    _

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -51,12 +51,6 @@ export default function Bloges() {
             ))}
             <Link href={"/"} className="btn btn-primary">
               Home
-            <Link href={"/"} className="btn btn-primary">
-              Home
-            </Link><Link href={"/"} className="btn btn-primary">
-              Home
-            </Link><Link href={"/"} className="btn btn-primary">
-              Home
             </Link>
           </div>
         </div>
