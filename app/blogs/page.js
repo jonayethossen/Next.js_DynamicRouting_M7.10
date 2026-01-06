df20882f3d528bd4b6a3be446fa1bf9ea87da950
jonayethossen <jonayethossen8921@gmail.com>
2026-01-06 15:12:36 +0600

    _

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -5,18 +5,6 @@ import HeroImg from "../../public/blogImg.png";
 import Image from "next/image";
 export default function Bloges() {
   return (
-    // <div className="p-8">
-    //   <h1 className="text-[60px] font-bold">Blog item</h1>
-    //   <div className="mt-6 p-4">
-    //     {blogs.map((blog) => (
-    //       <BlogItem key={blog.id} title={blog.title} />
-    //     ))}
-    //   </div>
-    //   <Link href={"/"} className="btn btn-primary">
-    //     {" "}
-    //     Home
-    //   </Link>
-    // </div>
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <Image
@@ -34,17 +22,7 @@ export default function Bloges() {
             voluptatem earum nemo. Ipsum eligendi ea exercitationem quisquam
             voluptas, quaerat rem voluptate quibusdam veniam?
           </p>
-          {/* <div className="space-x-2">
-            <Link href={"/blog1"} className="btn btn-primary">
-              Blog1
-            </Link>
-            <Link href={"/blog2"} className="btn btn-primary">
-              Blog2
-            </Link>
-            <Link href={"/blog3"} className="btn btn-primary">
-              Blog3
-            </Link>
-          </div> */}
+
           <div className=" flex gap-2">
             {blogs.map((blog) => (
               <BlogItem key={blog.id} title={blog.title} />
