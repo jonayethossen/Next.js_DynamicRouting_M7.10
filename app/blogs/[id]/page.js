14f3e26f22969a64c5acd71abbdc74d66ac31875
jonayethossen <jonayethossen8921@gmail.com>
2026-01-06 22:53:56 +0600

    GenerateStaticParams

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,13 +1,18 @@
 import blogs from "@/app/data/blog";
 import Link from "next/link";
 
+export function generateStaticParams() {
+  return blogs.map((blog) => ({
+    id: blog.id,
+  }));
+}
 export default async function SingleBlog({ params }) {
   const { id } = await params;
   const blog = blogs.find((blog) => blog.id === id);
   const { title, description } = blog;
 
   return (
-    <div className="mt-20 bg-black w-[600px] p-10">
+    <div className="mt-20 bg-black w-[600px] p-10 flex flex-col  justify-center mx-auto">
       <h1 className="text-xl font-bold text-teal-300">{title}</h1>
       <p className="mt-6">{description}</p>
       <Link href={"/"}>
